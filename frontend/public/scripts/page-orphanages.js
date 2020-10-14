9b3ff9eb5b5001748d2a0c519d60706860792ef0
pedro.santos <pedroabs.dev42@gmail.com>
2020-10-14 17:31:01 -0300

    Orphanages page completed

--- frontend/public/scripts/page-orphanages.js
+++ frontend/public/scripts/page-orphanages.js
@@ -1,8 +1,21 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([-8.063167, -34.8732721], 16);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
 
-var marker = L.marker([51.5, -0.09])
-    .addTo(mymap)
-    .bindPopup('A pretty CSS3 popup')
-    .openPopup();
+const icon = L.icon({
+    iconUrl: './public/images/map-marker.svg',
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2],
+})
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
+
+L.marker([-8.063167, -34.8732721], { icon })
+.addTo(map)
+.bindPopup(popup)    
